e5572a0aa43c509894f210384eb7e1536326947e
Tyo <138995711+tyodsgn@users.noreply.github.com>
2026-01-27 21:24:52 +0700

    Initial Commit

---
+++ NativeSankeyDiagram/NativeSankeyDiagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeSankeyDiagramApp.swift
+//  NativeSankeyDiagram
+//
+//  Created by Setyono Dwi Utomo on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct NativeSankeyDiagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
